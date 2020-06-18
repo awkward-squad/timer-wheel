d05461b1d918416fbb185d7760c5bbe002ed9008
Mitchell Rosen <mitchellwrosen@gmail.com>
2020-06-17 22:04:07 -0400

    Add a test

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BlockArguments, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE BlockArguments, DeriveAnyClass, DerivingStrategies, FlexibleInstances, MultiParamTypeClasses #-}
 
 import Control.Concurrent
 import Control.Exception
@@ -13,7 +13,7 @@ main :: IO ()
 main = do
   do
     putStrLn "Timer wheel runs scheduled actions"
-    with Config { spokes = 4, resolution = 0.25 } \wheel -> do
+    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
       var <- newEmptyMVar
       let n = 1000
       replicateM_ n (register_ wheel (putMVar var ()) 0)
@@ -22,7 +22,7 @@ main = do
   do
     putStrLn "Timers can be canceled"
     var <- newEmptyMVar
-    with Config { spokes = 4, resolution = 0.25 } \wheel -> do
+    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
       let n = 1000
       cancels <- replicateM n (register wheel (putMVar var ()) 0)
       successes <- sequence (take (n `div` 2) cancels)
@@ -30,14 +30,14 @@ main = do
 
   do
     putStrLn "Re-calling a successful cancel works"
-    with Config { spokes = 4, resolution = 0.25 } \wheel -> do
+    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
       cancel <- register wheel (pure ()) 1
       cancel `is` True
       cancel `is` True
 
   do
     putStrLn "Re-calling a failed cancel works"
-    with Config { spokes = 4, resolution = 0.25 } \wheel -> do
+    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
       var <- newEmptyMVar
       cancel <- register wheel (putMVar var ()) 0
       takeMVar var
@@ -55,6 +55,23 @@ main = do
       cancel -- should drop reference canary after a GC
       performGC
       (isJust <$> deRefWeak weakCanary) `is` False
+
+  do
+    putStrLn "`with` re-throws exception from background thread"
+    catch
+      (with Config { spokes = 4, resolution = 0.05 } \wheel -> do
+        var <- newEmptyMVar
+        register_ wheel (throwIO Bye >> putMVar var ()) 0
+        takeMVar var
+        throwIO (userError "fail"))
+      (\ex ->
+        case fromException ex of
+          Just Bye -> pure ()
+          _ -> throwIO ex)
+
+data Bye = Bye
+  deriving stock (Show)
+  deriving anyclass (Exception)
 
 class Assert a b where
   is :: a -> b -> IO ()
