5eeb936f2c0cea05e251d9b26f70e0b907517f18
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-11-05 16:19:42 +0530

    make test suite compile

--- test/Main.hs
+++ test/Main.hs
@@ -1,19 +1,23 @@
-{-# LANGUAGE BlockArguments, DeriveAnyClass, DerivingStrategies, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 import Control.Concurrent
 import Control.Exception
 import Control.Monad
 import Data.IORef
 import Data.Maybe (isJust)
-import Data.TimerWheel
 import System.Mem (performGC)
 import System.Mem.Weak (deRefWeak)
+import TimerWheel
 
 main :: IO ()
 main = do
   do
     putStrLn "Timer wheel runs scheduled actions"
-    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
+    with Config {spokes = 4, resolution = 0.05} \wheel -> do
       var <- newEmptyMVar
       let n = 1000
       replicateM_ n (register_ wheel 0 (putMVar var ()))
@@ -22,7 +26,7 @@ main = do
   do
     putStrLn "Timers can be canceled"
     var <- newEmptyMVar
-    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
+    with Config {spokes = 4, resolution = 0.05} \wheel -> do
       let n = 1000
       cancels <- replicateM n (register wheel 0 (putMVar var ()))
       successes <- sequence (take (n `div` 2) cancels)
@@ -30,14 +34,14 @@ main = do
 
   do
     putStrLn "Successful `cancel` returns True (then False)"
-    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
+    with Config {spokes = 4, resolution = 0.05} \wheel -> do
       cancel <- register wheel 1 (pure ())
       cancel `is` True
       cancel `is` False
 
   do
     putStrLn "Unsuccessful `cancel` returns False"
-    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
+    with Config {spokes = 4, resolution = 0.05} \wheel -> do
       var <- newEmptyMVar
       cancel <- register wheel 0 (putMVar var ())
       takeMVar var
@@ -45,7 +49,7 @@ main = do
 
   do
     putStrLn "Recurring timers work"
-    with Config { spokes = 4, resolution = 0.05 } \wheel -> do
+    with Config {spokes = 4, resolution = 0.05} \wheel -> do
       canary <- newIORef () -- kept alive only by timer
       weakCanary <- mkWeakIORef canary (pure ())
       var <- newEmptyMVar
@@ -58,15 +62,17 @@ main = do
   do
     putStrLn "`with` re-throws exception from background thread"
     catch
-      (with Config { spokes = 4, resolution = 0.05 } \wheel -> do
-        var <- newEmptyMVar
-        register_ wheel 0 (throwIO Bye >> putMVar var ())
-        takeMVar var
-        throwIO (userError "fail"))
-      (\ex ->
-        case fromException ex of
-          Just Bye -> pure ()
-          _ -> throwIO ex)
+      ( with Config {spokes = 4, resolution = 0.05} \wheel -> do
+          var <- newEmptyMVar
+          register_ wheel 0 (throwIO Bye >> putMVar var ())
+          takeMVar var
+          throwIO (userError "fail")
+      )
+      ( \ex ->
+          case fromException ex of
+            Just Bye -> pure ()
+            _ -> throwIO ex
+      )
 
 data Bye = Bye
   deriving stock (Show)
